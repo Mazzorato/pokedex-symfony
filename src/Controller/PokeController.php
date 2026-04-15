6b1d0ee3c0b47b54b8fbfd2fbdd08672dd88ccfd
Mazzorato <alexis.mazzorato@gmail.com>
2026-04-15 16:48:11 +0200

    first commit

--- src/Controller/PokeController.php
+++ src/Controller/PokeController.php
@@ -2,24 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Pokedex;
+use App\Repository\PokedexRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PokeController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(PokedexRepository $pokedexRepository): Response
     {
-        $nombrePokemons = 151;   
+        $pokemons = $pokedexRepository->findAll();
+         
         return $this->render('poke/index.html.twig',[
-        'nombrePokemons' => $nombrePokemons,
+        'nombrePokemons' => count($pokemons),
+        'pokemons' =>$pokemons,
         ]);
     }
     #[Route('/add',name:"app_add",methods:["GET","POST"])]
-    public function add(): Response
-    {
+    public function add(Request $request, EntityManagerInterface $entityManager): Response    {
+        // if ($request->isMethod('Post')){
+            // $name = $request->getPayload()->get('name');
+            // $imageUrl = $request->getPayload()->get('image_url');
+
+        $pokemon = new Pokedex();
+        $pokemon->setName("booba");
+        $pokemon->setImageUrl("http://unsplash.it/100/100");
+
+        $entityManager->persist($pokemon);
+        $entityManager->flush();
+        // }
+        return $this->render('poke/add.html.twig',[
         
-        return $this->render('poke/add.html.twig',);
+        ]);
     }
 }
